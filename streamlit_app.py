6928c3c9a509c2d771a0f3c41690fb3f3fc139d7
BINI-hishan <shanleighxyz@gmail.com>
2025-05-26 17:49:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,11 +37,9 @@ if ingredients_lists:
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write(f'The search value for {fruit_chosen} is {search_on}.')
 
-        st.header(f"{fruit_chosen} Nutrition Information")
-
-        # Fetch nutrition data
-        response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
-        fv_df = st.dataframe(data=response.json(),use _container_width = True)
+        st.header(fruit_chosen +'Nutrition Information')
+        fruityvice_response = requests.get(f"https://my.fruityvice.com/api/fruit/"+ search_on)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
 
     # Submit to Snowflake
     my_insert_stmt = f"""
