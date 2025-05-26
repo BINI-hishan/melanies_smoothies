8a3cd7a67e42689a1919a6a14e00d77201c626ab
BINI-hishan <shanleighxyz@gmail.com>
2025-05-26 14:17:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,48 @@
+# Import python packages
 import streamlit as st
-import snowflake.connector
-import pandas as pd
+from snowflake.snowpark.context import get_active_session
 
-st.title("Melanie's Snowflake Smoothies")
+from snowflake.snowpark.functions import col
 
-# Connect to Snowflake using secrets
-conn = snowflake.connector.connect(
-    user=st.secrets["SNOWFLAKE_USER"],
-    password=st.secrets["SNOWFLAKE_PASSWORD"],
-    account=st.secrets["SNOWFLAKE_ACCOUNT"],
-    warehouse=st.secrets["SNOWFLAKE_WAREHOUSE"],
-    database=st.secrets["SNOWFLAKE_DATABASE"],
-    schema=st.secrets["SNOWFLAKE_SCHEMA"],
+# Write directly to the app
+st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+st.write(
+  """Choose the fruits you want in your custom Smoothie!
+  """
 )
 
-# Example query to test connection
-query = "SELECT CURRENT_VERSION()"
 
-cur = conn.cursor()
-cur.execute(query)
-result = cur.fetchone()
 
-st.write(f"Connected to Snowflake version: {result[0]}")
+name_on_order = st.text_input('Name on Smoothie:')
+st.write("The name on your Smoothie will be: ", name_on_order)
 
-cur.close()
-conn.close()
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options"). select (col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingredients_lists = st.multiselect(
+    'Choose upto 5 ingredients: '
+    , my_dataframe
+    , max_selections=5
+) 
+
+if ingredients_lists:
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_lists:
+        ingredients_string += fruit_chosen + ' '
+
+   #st.write (ingredients_string)
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+
+
+  #st.write(my_insert_stmt)
+    time_to_insert = st.button('SubmitOrder')
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()   
+        st.success(f'Your Smoothie is ordered, {name_on_order}!')
+  
