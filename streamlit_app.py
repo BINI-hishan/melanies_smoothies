c9b37324d14873805131c10e1bec7defc54b7656
BINI-hishan <shanleighxyz@gmail.com>
2025-05-26 18:20:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if ingredients_lists:
       
         st.header(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
 
    #st.write (ingredients_string)
 
