6b7b3a7b4160dc4e68dd3fad177e0a7a58ba7ed7
BINI-hishan <shanleighxyz@gmail.com>
2025-05-26 17:40:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,17 +34,16 @@ if ingredients_lists:
     for fruit_chosen in ingredients_lists:
         ingredients_string += fruit_chosen + ' '
 
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write(f'The search value for {fruit_chosen} is {search_on}.')
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0].strip()
+        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
-        st.header(f"{fruit_chosen} Nutrition Information")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
 
-        # Fetch nutrition data
-        response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
-        if response.status_code == 200:
-            st.dataframe(data=response.json(), use_container_width=True)
-        else:
-            st.warning(f"No nutrition data found for {fruit_chosen}")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+    # st.write(ingredients_string)
+
 
     # Submit to Snowflake
     my_insert_stmt = f"""
